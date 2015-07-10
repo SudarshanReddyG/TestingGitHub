fa41e44e24956292b26c34bba77605e9132249a5
SudarshanReddyG <sudarshang.reddy@gmail.com>
2015-07-10 22:23:50 +0530

    First Commit

---
+++ TestGitHubRepository/TestingGitHub/src/com/sudarshan/github/FirstClass.java
@@ -0,0 +1,12 @@
+package com.sudarshan.github;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		SecondClass secClass = new SecondClass();
+		secClass.helloSecond();
+		
+		System.out.println("I am in First Class");
+	}
+
+}
---
+++ TestGitHubRepository/TestingGitHub/src/com/sudarshan/github/SecondClass.java
@@ -0,0 +1,9 @@
+package com.sudarshan.github;
+
+public class SecondClass {
+	
+	public void helloSecond() {
+		System.out.println("I am in second class");
+	}
+	
+}
